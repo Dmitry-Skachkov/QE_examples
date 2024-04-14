c4dee073cf8469c3eec9ae8759e600fef57cd8cf
Dmitry Skachkov <d.g.skachkov@gmail.com>
2024-04-14 18:58:37 -0500

    Update and rename plot_band_pdos.sh to src/plot_band_pdos.sh

--- plot_band_pdos.sh
+++
@@ -1,8 +0,0 @@
-# plot band + pdos
-
-qe_band_plot 1 21 41 61 -1.9505 0.000
-
-###pdos_int_qe B N  -3.49 1.228
-
-
- 
---
+++ src/plot_band_pdos.sh
@@ -0,0 +1,10 @@
+# plot band + pdos
+
+qe_band_plot 1 51 -0.7604 0.000
+
+###          numbers of high-symmetry points
+###                 EVBM   Eshift
+###pdos_int_qe B N  -3.49 1.228
+
+
+ 
